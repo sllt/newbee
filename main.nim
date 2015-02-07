0cfb36379d00ea9cf54e749f876eacb7ef835963
sllt <shenlongml@gmail.com>
2015-02-07 14:52:11 +0800

    add fileExists action

--- main.nim
+++ main.nim
@@ -1,10 +1,15 @@
 import asynchttpserver, asyncdispatch
+import os
 
 var server = newAsyncHttpServer()
 
 proc cb(req: Request) {.async.} =
-  let str = readFile(req.url.path[1.. -1])
-  await req.respond(Http200, str)
+  let file = req.url.path[1.. -1]
+  if fileExists(file) == true:
+    var str = readFile(file)
+    await req.respond(Http200, str)
+  else :
+    await req.respond(Http200, "<h1> 404 not found </h1>")
 
 asyncCheck server.serve(Port(8080), cb)
 runForever()
