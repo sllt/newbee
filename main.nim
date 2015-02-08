14df4409f17016689ad6b61ad7c598d6b43bc784
sllt <shenlongml@gmail.com>
2015-02-08 14:30:14 +0800

    add rootPath

--- main.nim
+++ main.nim
@@ -2,14 +2,15 @@ import asynchttpserver, asyncdispatch
 import os
 
 var server = newAsyncHttpServer()
+var rootPath = getCurrentDir().joinPath("web")
 
 proc cb(req: Request) {.async.} =
-  let file = req.url.path[1.. -1]
+  let file = rootPath.joinPath(req.url.path[1.. -1])
   if fileExists(file) == true:
     var str = readFile(file)
     await req.respond(Http200, str)
   else :
-    await req.respond(Http200, "<h1> 404 not found </h1>")
+    await req.respond(Http404, "<h1> 404 not found </h1>")
 
 asyncCheck server.serve(Port(8080), cb)
 runForever()
