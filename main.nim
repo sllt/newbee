0bb262038760b560a8e0d6f2e6e85398a37709f7
sllt <shenlongml@gmail.com>
2015-02-09 20:28:00 +0800

    add default page for server

--- main.nim
+++ main.nim
@@ -5,7 +5,14 @@ var server = newAsyncHttpServer()
 var rootPath = getCurrentDir().joinPath("web")
 
 proc cb(req: Request) {.async.} =
-  let file = rootPath.joinPath(req.url.path[1.. -1])
+  let uri = req.url.path[1.. -1]
+  if uri == "":
+    if fileExists(rootPath.joinPath("index.html")) == true:
+      var str = readFile(rootPath.joinPath("index.html"))
+      await req.respond(Http200, str)
+    else:
+      await req.respond(Http404, "<h1> 404 not found </h1>")
+  let file = rootPath.joinPath(uri)
   if fileExists(file) == true:
     var str = readFile(file)
     await req.respond(Http200, str)
