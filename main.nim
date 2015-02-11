84d48655a888e14af3429d5fd69ff279bd0085b2
sllt <shenlongml@gmail.com>
2015-02-11 16:43:03 +0800

    add hostname for server

--- main.nim
+++ main.nim
@@ -3,15 +3,16 @@ import os
 
 var server = newAsyncHttpServer()
 var rootPath = getCurrentDir().joinPath("web")
-
 proc cb(req: Request) {.async.} =
+  echo req.hostname
   let uri = req.url.path[1.. -1]
   if uri == "":
     if fileExists(rootPath.joinPath("index.html")) == true:
       var str = readFile(rootPath.joinPath("index.html"))
       await req.respond(Http200, str)
     else:
-      await req.respond(Http404, "<h1> 404 not found </h1>")
+      var dir  = getCurrentDir().joinPath("web")
+      await req.respond(Http200, "str")
   let file = rootPath.joinPath(uri)
   if fileExists(file) == true:
     var str = readFile(file)
@@ -19,5 +20,6 @@ proc cb(req: Request) {.async.} =
   else :
     await req.respond(Http404, "<h1> 404 not found </h1>")
 
+echo "server start on http://0.0.0.0:8080"
 asyncCheck server.serve(Port(8080), cb)
 runForever()
